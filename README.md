755a3f2e85d27c57240b69b9ed57086ba51e2760
lowy-10pines <martin.loguancio@10pines.com>
2023-09-14 11:56:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fancy Theme mode switch
+
+Un switch facherito para cambiar de modo oscuro a modo claro.
+
+![ejemplo](https://github.com/lowy-10pines/flutter-fancy-theme-mode-switch/blob/main/assets/recording.gif?raw=true)
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FancySwitchApp());
+}
+
+class FancySwitchApp extends StatefulWidget {
+  const FancySwitchApp({super.key});
+
+  @override
+  State<FancySwitchApp> createState() => _FancySwitchAppState();
+}
+
+class _FancySwitchAppState extends State<FancySwitchApp> {
+  bool value = true;
+
+  void _setValue(bool newValue) {
+    setState(() {
+      value = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fancy Switch demo',
+      themeMode: value ? ThemeMode.light : ThemeMode.dark,
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      theme: ThemeData.light(useMaterial3: true),
+      home: Scaffold(
+        body: Center(
+          child: Transform.scale(
+            scale: 4,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Switch.adaptive(value: value, onChanged: _setValue),
+                const SizedBox(width: 50, height: 30, child: Placeholder())
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
